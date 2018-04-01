620c8abbdcd6bc21e43e1252b1202c024e3bbcc7
Andrea Lambruschini <andrea.lambruschini@gmail.com>
2018-04-01 21:22:32 +0200

    Primo Commit

---
+++ include/pru_mpu6050_driver.h
@@ -0,0 +1,15 @@
+/*
+ * pru_mpu6050_driver.h
+ *
+ *  Created on: 01 apr 2018
+ *      Author: andrea
+ */
+
+#ifndef PRU_MPU6050_DRIVER_H_
+#define PRU_MPU6050_DRIVER_H_
+
+
+
+
+
+#endif /* PRU_MPU6050_DRIVER_H_ */
